f157845c2aac3ed42867f65ccb5fff472aa1b8de
Henrikas Svidras <49688132+henrikas-svidras@users.noreply.github.com>
2023-12-06 09:08:54 +0100

    final cleanup of comments

--- advent_of_code/2023/6_boat_races.py
+++ advent_of_code/2023/6_boat_races.py
@@ -2,6 +2,9 @@
 import numpy as np
 task_file = "advent_of_code/2023/inputs/6_task.txt"
 
+## Switch between different methods in the loop by commenting/uncommenting:
+## Commented part = brute force solution
+## Uncommented part = solution using a quadratic equation
 
 ## Part 1
 
@@ -10,11 +13,7 @@ with open(task_file) as f:
     times = [int(line) for line in lines[0] if len(line)>0]
     distances = [int(line) for line in lines[1] if len(line)>0]
 
-# print(times)
-# print(distances)
-
-# wins = []
-wins_eqn = []
+wins = []
 for n, (time, distance) in enumerate(zip(times, distances)):
     # wins.append(0)
     # for hold_time in range(1, time):
@@ -25,12 +24,11 @@ for n, (time, distance) in enumerate(zip(times, distances)):
     #         wins[n]+=1
     
     roots = np.roots([1,-1*time, distance])
-    wins_eqn.append(np.ceil(max(roots)-0.001) - np.ceil(min(roots)+0.001))
+    wins.append(np.ceil(max(roots)-0.001) - np.ceil(min(roots)+0.001))
     
 
-# print("Wins in races ", wins)
-# print("Answer ", np.prod(wins))
-print(np.prod(wins_eqn))
+print("Wins in races ", wins)
+print("Answer ", np.prod(wins))
 
 
 ## Part 2
@@ -39,13 +37,8 @@ with open(task_file) as f:
     lines = [line.replace("\n","").split(": ")[1].split(" ") for line in f]
     times = [int("".join([line for line in lines[0] if len(line)>0]))]
     distances = [int("".join([line for line in lines[1] if len(line)>0]))]
-    
 
-# print(times)
-# print(distances)
-
-# wins = []
-wins_eqn = []
+wins = []
 
 for n, (time, distance) in enumerate(zip(times, distances)):
     # wins.append(0)
@@ -56,6 +49,7 @@ for n, (time, distance) in enumerate(zip(times, distances)):
     #     if new_distance > distance:
     #         wins[n]+=1
     roots = np.roots([1,-1*time, distance])
-    wins_eqn.append(np.ceil(max(roots)-0.001) - np.ceil(min(roots)+0.001))
+    wins.append(np.ceil(max(roots)-0.001) - np.ceil(min(roots)+0.001))
 
-print(np.prod(wins_eqn))
+print("Wins in races ", wins)
+print("Answer ", np.prod(wins))

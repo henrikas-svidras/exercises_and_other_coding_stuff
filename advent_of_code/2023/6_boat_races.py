a140f4718724434a1a8f9067527ec0abb76dbb42
Henrikas Svidras <49688132+henrikas-svidras@users.noreply.github.com>
2023-12-06 08:58:38 +0100

    solvable with a quadratic equation

--- advent_of_code/2023/6_boat_races.py
+++ advent_of_code/2023/6_boat_races.py
@@ -10,22 +10,29 @@ with open(task_file) as f:
     times = [int(line) for line in lines[0] if len(line)>0]
     distances = [int(line) for line in lines[1] if len(line)>0]
 
-print(times)
-print(distances)
+# print(times)
+# print(distances)
 
 beats = False
-wins = []
-
+# wins = []
+wins_eqn = []
 for n, (time, distance) in enumerate(zip(times, distances)):
-    wins.append(0)
-    for hold_time in range(1, time):
-        left_time = time - hold_time
-        speed = hold_time
-        new_distance = speed * left_time
-        if new_distance > distance:
-            wins[n]+=1
-print(wins)
-print(np.prod(wins))
+    # wins.append(0)
+    # for hold_time in range(1, time):
+    #     left_time = time - hold_time
+    #     speed = hold_time
+    #     new_distance = speed * left_time
+    #     if new_distance > distance:
+    #         wins[n]+=1
+    
+    roots = np.roots([1,-1*time, distance])
+    wins_eqn.append(np.ceil(max(roots)-0.001) - np.ceil(min(roots)+0.001))
+    
+
+# print("Wins in races ", wins)
+# print("Answer ", np.prod(wins))
+print(np.prod(wins_eqn))
+
 
 ## Part 2
 
@@ -35,19 +42,22 @@ with open(task_file) as f:
     distances = [int("".join([line for line in lines[1] if len(line)>0]))]
     
 
-print(times)
-print(distances)
+# print(times)
+# print(distances)
 
 beats = False
-wins = []
+# wins = []
+wins_eqn = []
 
 for n, (time, distance) in enumerate(zip(times, distances)):
-    wins.append(0)
-    for hold_time in range(1, time):
-        left_time = time - hold_time
-        speed = hold_time
-        new_distance = speed * left_time
-        if new_distance > distance:
-            wins[n]+=1
-print(wins)
-print(np.prod(wins))
+    # wins.append(0)
+    # for hold_time in range(1, time):
+    #     left_time = time - hold_time
+    #     speed = hold_time
+    #     new_distance = speed * left_time
+    #     if new_distance > distance:
+    #         wins[n]+=1
+    roots = np.roots([1,-1*time, distance])
+    wins_eqn.append(np.ceil(max(roots)-0.001) - np.ceil(min(roots)+0.001))
+
+print(np.prod(wins_eqn))

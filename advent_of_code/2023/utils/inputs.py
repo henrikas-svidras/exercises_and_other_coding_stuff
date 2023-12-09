cfdad59f696e114fea1998fffab0c50c47e826d2
Henrikas Svidras <henrikas.svidras@desy.de>
2023-12-09 19:10:10 +0100

    also adding code to fetch test data

--- advent_of_code/2023/utils/inputs.py
+++ advent_of_code/2023/utils/inputs.py
@@ -3,17 +3,16 @@ import os
 file_path = os.path.abspath(__file__)
 inputs_dir = os.path.dirname(os.path.dirname(file_path)) + "/inputs"
 
-def is_data_present(year, day):
-    return os.path.exists(inputs_dir+f"/{day}_task.txt")
+def is_data_present(path):
+    return os.path.exists(path)
 
 def get_data_set(year, day):
     path = inputs_dir+f"/{day}_task.txt"
-    if is_data_present(year, day):
+    if is_data_present(path):
         print("Downloaded already. Loading...")
         with open(path) as f:
             lines = [line.replace("\n","") for line in f]
         return lines
-    
     else:        
         import urllib.request
         import dotenv
@@ -29,3 +28,23 @@ def get_data_set(year, day):
         with open(path, "wb") as f:
             f.write(page)
         return get_data_set(year, day)
+
+def get_test_data_set(year, day):
+    path = inputs_dir+f"/{day}_task_test.txt"
+    if is_data_present(path):
+        print("Downloaded already. Loading test data...")
+        with open(path) as f:
+            lines = [line.replace("\n","") for line in f]
+        return lines
+    else:        
+        import urllib.request
+        import re
+        url = f'https://adventofcode.com/{year}/day/{day}'
+        print(f"Downloading testa data from {url}...")
+        a_request = urllib.request.Request(url)
+        page = urllib.request.urlopen(a_request).read()
+        result = page.split(b"For example:")[1]
+        result = re.search(rb"<pre><code>(.*?)</code></pre>", result, re.DOTALL).group(1)
+        with open(path, "wb") as f:
+            f.write(result)
+        return get_test_data_set(year, day)

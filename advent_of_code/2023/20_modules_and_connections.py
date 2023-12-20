3bc5472e850492f9c0658c3d9f06c3de5424ed29
Henrikas Svidras <49688132+henrikas-svidras@users.noreply.github.com>
2023-12-20 22:30:17 +0100

    cleaned up and commented

--- advent_of_code/2023/20_modules_and_connections.py
+++ advent_of_code/2023/20_modules_and_connections.py
@@ -10,17 +10,24 @@ data = get_data_set(2023,20)
 class Pulse:
     high = False
     low = True
+    tap = 0
     def __init__(self, is_high=False):
         if is_high:
             self.high = True
         else:
             self.low = True
+    def __repr__(self):
+        return "high" if self.high else "low"
 
-flipflops = {}
 conjunctions = {}
+flipflops = {}
 outputs = {}
 
 # Parse
+# IDEA: 
+# flip flops: are a dictionary of True / False (on/off state)
+# conjunctions: dictionary of dictionaries, containing outputs that are connected to this conjunction.
+# outputs: what is connected to each input
 for line in data:
     split_line = line.split(" -> ")
     name = split_line[0]
@@ -33,8 +40,9 @@ for line in data:
         outputs[name[1:]] = connections
     else:
        outputs[name] = connections
-
-# Set what connects to conjunction
+        
+# Set what connects to each conjunction, for flip flop irrelevant
+# For connected last default state is False (last pulse "low")
 for conjunction_name in conjunctions.keys():
     for name, output_names in outputs.items():
         if conjunction_name in output_names:
@@ -44,11 +52,10 @@ def part1and2(connection_to_rx):
 
     high_count = 0
     low_count = 0
-    counts = defaultdict(lambda:[])
+    period_counts = defaultdict(lambda:[])
     start = time.time()
 
-    for _ in range(10000000000):
-        
+    for _ in range(10000000000): # this will break much earlier
         #For part1 solution :
         if _==1000:
             print(f"{(time.time() - start):.2f}s")
@@ -56,16 +63,16 @@ def part1and2(connection_to_rx):
 
 
         #For part2 solution :
-        if len(counts)>0 and all(len(period)>=2 for period in counts.values()):
-            result = math.lcm(*list([count2-count1 for count1, count2 in counts.values()]))
+        if len(period_counts)>0 and all(len(period)>=2 for period in period_counts.values()):
+            result = math.lcm(*list([count2-count1 for count1, count2 in period_counts.values()]))
             print(f"{(time.time() - start):.2f}s")
             return result
         
-        #Main loop        
-        queue = [ ('button', 'broadcaster', Pulse(False)) ]
+        #Main loop      
+        # queue contains: sender module, receiver module and pulse type (True = High), (False=Low)
+        queue = [ (None, "broadcaster", Pulse(False)) ]
 
         while queue:
-
             sender, receiver, pulse = queue.pop(0)
 
             if pulse.high:
@@ -74,9 +81,12 @@ def part1and2(connection_to_rx):
                 low_count += 1
             
             ## Module logic:
-            if receiver == 'broadcaster':
-                for connection in outputs[receiver]:
-                    queue.append((receiver, connection, pulse))
+            # Initial input
+            if not sender:
+                for connection in outputs["broadcaster"]:
+                    queue.append( (receiver, connection, pulse) )
+
+            # Deal with flip flop
             elif receiver in flipflops:
                 if pulse.high:
                     continue
@@ -89,13 +99,9 @@ def part1and2(connection_to_rx):
                             queue.append( (receiver, connection, Pulse(False)) )
 
                     flipflops[receiver] = not flipflops[receiver]
+            # Deal with conjunctions
             elif receiver in conjunctions:
-
-                conjunctions[receiver][sender] = pulse.high
-
-                #For part2 searching for period:
-                if receiver == connection_to_rx and pulse.high:
-                   counts[sender].append(_)
+                conjunctions[receiver][sender] = pulse.high # this is True if high, False if low
 
                 if all(conjunctions[receiver].values()):
                     for connection in outputs[receiver]:
@@ -104,5 +110,15 @@ def part1and2(connection_to_rx):
                     for connection in outputs[receiver]:
                         queue.append( (receiver, connection, Pulse(True) ) )
 
+            #For part2 searching for period:
+            # There is a Conjunction connection to my "rx" and 4 other connections to the conjunction.
+            # It means I need to find when all of them will align and give a high pulse.
+            # This means finding a period, and then LCM.
+            # This can also be done by printing "sender" and "_" and then manually stopping, and finding the periods.
+            # I did it like that initially :D
+            # For pulse to reach rx, all of the 4 need to get a high pulse. 
+            # I am here marking when this happens to later calculate the period (see before while loop)
+            if receiver == connection_to_rx and pulse.high:
+                period_counts[sender].append(_)
 
 print(part1and2("vr"))

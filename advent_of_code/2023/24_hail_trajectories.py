97fb76da6156ea0151ee627b736c2d0a46d0144b
Henrikas Svidras <49688132+henrikas-svidras@users.noreply.github.com>
2023-12-24 12:29:56 +0100

    cleaned up code

--- advent_of_code/2023/24_hail_trajectories.py
+++ advent_of_code/2023/24_hail_trajectories.py
@@ -7,56 +7,55 @@ import collections
 
 data = get_data_set(2023,24)
 
-def will_pass_through_same_point(x1, y1, v1_x, v1_y, x2, y2, v2_x, v2_y, lims = (200000000000000, 400000000000000)):
-    # Check if the velocities are zero (stationary particles)
+def will_pass_through_same_point(x1, y1, v1_x, v1_y, x2, y2, v2_x, v2_y, lims):
+    
+    if (x1, y1) == (x2, y2):
+        return True
+    
+    # Not moving
     if v1_x == v1_y == v2_x == v2_y == 0:
-        return True if (x1, y1) == (x2, y2) else False
+        return False
+    
+    # Parallel condition (cross produt)
+    if not (v1_x * v2_y - v1_y * v2_x):
+        return False
 
-    # Check for parallel trajectories (no intersection if starting points are different)
-    if v1_x * v2_y == v1_y * v2_x:
-        return True if (x1, y1) == (x2, y2) else False
+    slope1 = v1_y / v1_x
+    slope2 = v2_y / v2_x
+    # If they have the same slope they intersect by definition
+    if slope1 == slope2:
+        return True
 
-    # Solving the line equations for intersection
-    try:
-        # Calculate the slopes and check if they are equal (parallel lines)
-        slope1 = v1_y / v1_x
-        slope2 = v2_y / v2_x
-        if slope1 == slope2:
-            return True
+    # Just simple calculations I did on paper, easy to prove by writing it out
+    x_intersect = (slope1 * x1 - slope2 * x2 - y1 + y2) / (slope1 - slope2)
+    y_intersect = slope1 * (x_intersect - x1) + y1
 
-        # Calculate the intersection point
-        x_intersect = (slope1 * x1 - slope2 * x2 - y1 + y2) / (slope1 - slope2)
-        y_intersect = slope1 * (x_intersect - x1) + y1
+    # Put all values in t = d / v equation
+    time_of_cross_1 = (x_intersect - x1) / v1_x 
+    time_of_cross_2 = (x_intersect - x2) / v2_x 
 
-        time_of_cross_1 = (x_intersect - x1) / v1_x 
-        time_of_cross_2 = (x_intersect - x2) / v2_x 
-
-        if lims[1]>=x_intersect >=lims[0] and lims[1]>=y_intersect>=lims[0] and time_of_cross_1>=0 and time_of_cross_2>=0:
-            return True
-        else:
-            return False
-    except ZeroDivisionError:
-        # Handle division by zero (vertical lines)
-        return True if x1 == x2 else False
+    # Check for limits and time
+    if lims[1]>=x_intersect >=lims[0] and lims[1]>=y_intersect>=lims[0] and time_of_cross_1>=0 and time_of_cross_2>=0:
+        return True
+    else:
+        return False
 
 
-def part1(position, velocities):
+def part1(positions, velocities):
     combs = combinations(positions, 2)
     combs_vel = combinations(velocities, 2)
-
     cross = []
 
     for (hail1, hail2), (vel1, vel2) in zip(combs, combs_vel):
-        x1, y1, z1 = hail1
-        x2, y2, z2 = hail2
-        v1_x, v1_y, v1_z = vel1
-        v2_x, v2_y, v2_z = vel2
+        x1, y1, _ = hail1
+        x2, y2, _ = hail2
+        v1_x, v1_y, _ = vel1
+        v2_x, v2_y, _ = vel2
 
-        cross.append(will_pass_through_same_point(x1, y1, v1_x, v1_y, x2, y2, v2_x, v2_y))
-
+        #Change lims for test data
+        cross.append(will_pass_through_same_point(x1, y1, v1_x, v1_y, x2, y2, v2_x, v2_y, lims = (200000000000000, 400000000000000)))
+    
     return sum(cross)
-
-
 
 # Parse data
 
@@ -96,21 +95,21 @@ def equations(p, *vals):
 def part2(positions, velocities):
     sums = []
     for val in range(len(positions)-2):
-        x, y, z, xv, yv, zv = fsolve(equations, (positions[-1], velocities[-1]), args=(val,val+3))
-        sums.append(round(x+y+z,10))
+        x, y, z, _, _, _ = fsolve(equations, (positions[-1], velocities[-1]), args=(val,val+3))
+        sums.append(round(x+y+z,2))
     return sums
 
 
 start_time = time.time()
 
 results = part2(positions, velocities)
-print(results)
-print(collections.Counter(results).items())
-print([item for item, count in collections.Counter(results).items() if count > 1])
-print(collections.Counter(results).most_common()[0][0])
+# For debug
+# print(results)
+# print(collections.Counter(results).items())
+# print([item for item, count in collections.Counter(results).items() if count > 3])
 
 print(f"Took: {(time.time() - start_time):.2f}s")
-print("Part 2 answer: ", collections.Counter(results).most_common()[0][0])
+print("Part 2 answer: ", int(collections.Counter(results).most_common()[0][0]))
 
 # ## Part 2 (FOUND ON INTERNET --> More straightforward solution, I guess)
 # from z3 import Solver, Int, Real, sat
@@ -132,4 +131,3 @@ print("Part 2 answer: ", collections.Counter(results).most_common()[0][0])
 
 # print(f"Took: {(time.time() - start_time):.2f}s")
 # print("Part 2 answer: ", sum(int(str(m.evaluate(v))) for v in P))
-

889541ced751869460324f7c37cd54967529a1cd
Henrikas Svidras <svidras@gmail.com>
2024-12-04 23:33:53 +0100

    remove random test file

--- advent_of_code/2015/test.py
+++
@@ -1,9 +0,0 @@
-from itertools import combinations, permutations
-
-test = [1,2,3]
-
-for i in range(3):
-    print([comb for comb in combinations(test, i)])
-
-for i in range(3):
-    print([comb for comb in permutations(test, i)])

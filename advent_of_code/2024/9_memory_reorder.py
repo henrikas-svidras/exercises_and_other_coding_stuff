553bf0a35d28ffc51185a7bf04358410195de987
Henrikas Svidras <svidras@gmail.com>
2024-12-09 08:02:36 +0100

    skull emoji

---
+++ advent_of_code/2024/9_memory_reorder.py
@@ -0,0 +1,70 @@
+## 9th task of advent of code
+from utils.inputs import get_data_set, get_test_data_set
+import time
+
+data = get_data_set(2024,9)
+
+
+def process_data(data=data, for_part_1=True):
+
+    fil = 0  
+    pos = 0    
+    
+    file_map = []
+    empty_space = [] 
+    layout = []     
+  
+
+    for i, char in enumerate(data[0]):
+        mem_size = int(char)
+        if i % 2 == 0: 
+            if not for_part_1:
+                file_map.append((pos, mem_size, fil))
+            for _ in range(mem_size):
+                if for_part_1:
+                    file_map.append((pos, 1, fil))
+                pos += 1
+                layout.append(fil)
+            fil += 1
+        else:  
+            empty_space.append((pos, mem_size))
+            layout += [None] * mem_size
+            pos += mem_size
+
+    # print(file_map)
+    # print(empty_space)
+    # print(layout)
+    # print("---")
+    return file_map, empty_space, layout
+
+inp1 = process_data()
+inp2 = process_data(for_part_1=False)
+
+def part1(inp):
+    file_map, free_spaces,layout = inp    
+    
+    for pos, size, file_id in reversed(file_map):
+        for n, (space_pos, space_size) in enumerate(free_spaces):
+            if space_pos < pos and size <= space_size:
+                for offset in range(size):
+                    layout[space_pos +offset] = file_id
+                    layout[pos +offset] = None
+
+                free_spaces[n] = (space_pos+size,space_size-size)
+                break
+
+    return sum(n * val for n, val in enumerate(layout) if val)
+
+def part2(inp):
+
+    return part1(inp)
+
+start = time.time()
+
+res1 = part1(inp1)
+print(f"Part 1 took: {(time.time() - start):.2f}s")
+print(f"Result of part 1: {res1}")
+
+res2 = part2(inp2)
+print(f"Part 2 took: {(time.time() - start):.2f}s")
+print(f"Result of part 2: {res2}")

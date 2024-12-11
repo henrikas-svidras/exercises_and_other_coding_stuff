1629651f13737c5d15a09ceb8d376a6ef5a3ba8c
Henrikas Svidras <svidras@gmail.com>
2024-12-11 20:10:06 +0100

    add number splitting helper

--- advent_of_code/2024/utils/helpers.py
+++ advent_of_code/2024/utils/helpers.py
@@ -1,5 +1,12 @@
 from functools import reduce
 from operator import mul
+import math
 
 def prod(it):
     return reduce(mul, it)
+
+def split_even_number(x):
+    l = math.floor(math.log10(x)) +1    
+    if l%2 == 0:
+        return x // 10**(l//2), x % 10**(l//2)
+    return x

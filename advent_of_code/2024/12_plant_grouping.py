2ca9526122b873ce20fec6421510c1430ccf3035
Henrikas Svidras <svidras@gmail.com>
2024-12-14 00:14:34 +0100

    clean up 12th day

--- advent_of_code/2024/12_plant_grouping.py
+++ advent_of_code/2024/12_plant_grouping.py
@@ -28,23 +28,20 @@ def process_data(data=data):
 inp = process_data()
 
 def analyze_region(start, seen, full_grid):
-    plant = full_grid[start]
+    
     plants_in_perimeter = [start]
-   
-    perimeter = set()
     checked_area = set()
-   
+    perimeter = set()
     for pos in plants_in_perimeter:
         checked_area.add(pos)
         for direction in DIRS_COMPLEX[:4]:
             neighbour = pos + direction
-            if neighbour not in checked_area:
-                if (neighbour in full_grid) and (full_grid[neighbour] == plant):
+            if not neighbour in checked_area:
+                if neighbour in full_grid and full_grid[neighbour] == full_grid[start]:
                     seen.add(neighbour)
-                    checked_area.add(neighbour)
                     plants_in_perimeter.append(neighbour)
-                    continue
-                else: # if neighbour isnt in grid or wrong plant type then this is either grid end, or a different plant, so an "edge"
+                    checked_area.add(neighbour)
+                else:
                     perimeter.add((neighbour, direction))
     return plants_in_perimeter, perimeter
 
@@ -58,12 +55,10 @@ def count_sides(perimeter):
         plants_in_perimeter = [per_pos]
         checked_perimeter.add((per_pos, check_dir))
         for pos in plants_in_perimeter:
-            for delta in (check_dir*-1j , check_dir*1j): # check only 90 deg to not make turns
+            for delta in (check_dir*-1j , check_dir*1j):
                 neighbour = pos + delta
-                if (neighbour, check_dir) not in checked_perimeter:
+                if not (neighbour, check_dir) in checked_perimeter:
                     if (neighbour, check_dir) in perimeter:
-                        # will add all the values tto the checked perimeter until you leave the full perimeter
-                        # so side counts wont count again for this direction
                         plants_in_perimeter.append(neighbour)
                         checked_perimeter.add((neighbour, check_dir))
 
@@ -78,8 +73,9 @@ def part12(inp):
         area, perimeter = analyze_region(loc, seen, inp)
         side_count = count_sides(perimeter)
 
-        ans1 += len(area)*len(perimeter)
+        ans1+= len(area)*len(perimeter)
         ans2 += side_count * len(area)
+
     
     return ans1, ans2
 
